d5c467ebfe71b6c585752ed7c859ce62d6b5704c
kelin <kelin.tan@alo7.com>
2019-06-17 13:55:55 +0800

    support mock test

--- src/test/java/com/jrs/kelin/BaseTest.java
+++ src/test/java/com/jrs/kelin/BaseTest.java
@@ -5,6 +5,7 @@ package com.jrs.kelin;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -19,9 +20,9 @@ import org.springframework.web.context.WebApplicationContext;
  * @author Kelin Tan
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles("test")
+@ActiveProfiles("test") //test环境
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@Transactional
+@Transactional //添加Transactional注解进行数据回滚，隔离多个单元测试环境
 @Ignore
 public class BaseTest {
     @Autowired
@@ -31,6 +32,9 @@ public class BaseTest {
 
     @Before
     public void setUp() {
+        //注入mockMvc用于rest api测试
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        //初始化mock注解支持
+        MockitoAnnotations.initMocks(this);
     }
 }
---
+++ src/test/java/com/jrs/kelin/controller/UserControllerMockTest.java
@@ -0,0 +1,42 @@
+// Copyright 2019 Alo7 Inc. All rights reserved.
+
+package com.jrs.kelin.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.jrs.kelin.BaseTest;
+import com.jrs.kelin.entity.User;
+import com.jrs.kelin.mapper.UserMapper;
+import org.assertj.core.util.Lists;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+/**
+ * @author Kelin Tan
+ */
+public class UserControllerMockTest extends BaseTest {
+    @Mock
+    @Autowired
+    UserMapper userMapper;
+
+    @InjectMocks
+    @Autowired
+    UserController userController;
+
+    @Test
+    public void mockUserMapperTest() throws Exception {
+        when(userMapper.findAll()).thenReturn(Lists.newArrayList(new User("mock", 2L)));
+
+        mockMvc.perform(get("/user/findAll").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value("1"))
+                .andExpect(jsonPath("$[0].id").value("2"))
+                .andExpect(jsonPath("$[0].userName").value("mock"));
+    }
+}
