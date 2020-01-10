94607b60572c021bece88c6d3f551300a7af1582
kelin <kelin.tan@alo7.com>
2020-01-10 15:54:11 +0800

    support more field type test

--- archetype-api/src/test/resources/data/biz/biz_account.sql
+++ archetype-api/src/test/resources/data/biz/biz_account.sql
@@ -1,2 +1,4 @@
-INSERT INTO biz_account(id,account,password,token) VALUES (1,'test1','password1','token1');
-INSERT INTO biz_account(id,account,password,token) VALUES (2,'test2','password2','token2');
+INSERT INTO `biz_account`(`id`, `account`, `password`, `token`, `age`, `height`, `money`, `birth_date`, `verify`) VALUES
+ (1, 'test1', 'password1', 'token1', 10, 20, 100.00, '2020-01-10 15:34:27', 1);
+INSERT INTO `biz_account`(`id`, `account`, `password`, `token`, `age`, `height`, `money`, `birth_date`, `verify`)
+VALUES (2, 'test2', 'password2', 'token2', 20, 30, 99.99, '2020-01-14 15:34:32', 0);
