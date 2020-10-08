e46aa04639717105a3df59c1e2e69cee09089a5f
kelin.tan <kelin.tan@alo7.com>
2020-10-08 11:01:02 +0800

    add mock user service test

---
+++ archetype-demo-api/src/test/java/com/kelin/archetype/api/service/UserServiceMockTest.kt
@@ -0,0 +1,33 @@
+// Copyright 2020 Kelin Inc. All rights reserved.
+
+package com.kelin.archetype.api.service
+
+import com.kelin.archetype.api.SpringBootArchetypeApplication
+import com.kelin.archetype.database.mapper.primary.UserMapper
+import com.kelin.archetype.test.KtBaseSpringTest
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.springframework.boot.test.context.SpringBootTest
+
+/**
+ * @author Kelin Tan
+ */
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.NONE,
+    classes = [SpringBootArchetypeApplication::class]
+)
+class UserServiceMockTest : KtBaseSpringTest() {
+    @InjectMocks
+    lateinit var userService: UserServiceImpl
+
+    @Mock
+    lateinit var userMapper: UserMapper
+
+    @Test
+    fun `find user by id with mock`() {
+        Mockito.`when`(userMapper.findOne(2)).thenReturn(null)
+        userService.findUserById(2) eq null
+    }
+}
