a31082cc2214590f1aacfdddbd199ca1d920515c
kelin.tan <kelin.tan@alo7.com>
2021-01-04 15:20:56 +0800

    add CollectionHelper to support batch command

---
+++ archetype-common/src/main/java/com/kelin/archetype/common/utils/CollectionHelper.java
@@ -0,0 +1,68 @@
+package com.kelin.archetype.common.utils;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * @author Kelin Tan
+ */
+public class CollectionHelper {
+    private static final Integer DEFAULT_BATCH_SIZE = 100;
+
+    public static <T, R> List<R> batchCall(List<T> list, Function<List<T>, R> caller) {
+        return batchCall(list, DEFAULT_BATCH_SIZE, caller);
+    }
+
+    public static <T> void batchConsume(List<T> list, Consumer<List<T>> consumer) {
+        batchConsume(list, DEFAULT_BATCH_SIZE, consumer);
+    }
+
+    public static <T> void batchConsume(List<T> list, int batchSize, Consumer<List<T>> consumer) {
+        if (CollectionUtils.isEmpty(list) || batchSize <= 0) {
+            return;
+        }
+
+        if (list.size() <= batchSize) {
+            consumer.accept(list);
+        }
+
+        int fromIndex = 0;
+        while (fromIndex < list.size()) {
+            int endIndex = fromIndex + batchSize;
+            //copy new list,do not operate sub list
+            List<T> subList = Lists.newArrayList(list.subList(fromIndex, Math.min(endIndex, list.size())));
+            consumer.accept(subList);
+            fromIndex = fromIndex + batchSize;
+        }
+    }
+
+    public static <T, R> List<R> batchCall(List<T> list, int batchSize, Function<List<T>, R> caller) {
+        if (CollectionUtils.isEmpty(list) || batchSize <= 0) {
+            return Collections.emptyList();
+        }
+
+        List<R> result = new ArrayList<>();
+        if (list.size() <= batchSize) {
+            result.add(caller.apply(list));
+            return result;
+        }
+
+        int fromIndex = 0;
+
+        while (fromIndex < list.size()) {
+            //copy new list,do not operate sub list
+            int endIndex = fromIndex + batchSize;
+            List<T> subList = Lists.newArrayList(list.subList(fromIndex, Math.min(endIndex, list.size())));
+            result.add(caller.apply(subList));
+            fromIndex = fromIndex + batchSize;
+        }
+        return result;
+    }
+
+}
---
+++ archetype-common/src/test/java/com/kelin/archetype/common/utils/CollectionHelperTest.kt
@@ -0,0 +1,123 @@
+package com.kelin.archetype.common.utils
+
+import com.kelin.archetype.test.KtTestUtils
+import org.junit.Test
+
+/**
+ * @author Kelin Tan
+ */
+class CollectionHelperTest : KtTestUtils {
+    @Test
+    fun `call in batch`() {
+        CollectionHelper.batchCall(listOf(1, 2, 3, 4), 2) { list ->
+            list.sum()
+        } verify {
+            item(0) eq 3
+            item(1) eq 7
+        }
+
+        CollectionHelper.batchCall(listOf(1, 2, 3), 2) { list ->
+            list.sum()
+        } verify {
+            item(0) eq 3
+            item(1) eq 3
+        }
+
+        CollectionHelper.batchCall(listOf(1, 2), 2) { list ->
+            list.sum()
+        } verify {
+            item(0) eq 3
+        }
+
+        CollectionHelper.batchCall(listOf(1), 2) { list ->
+            list.sum()
+        } verify {
+            item(0) eq 1
+        }
+
+        CollectionHelper.batchCall(listOf(1, 2, 3, 4, 5), 2) { list ->
+            list.sum()
+        } verify {
+            item(0) eq 3
+            item(1) eq 7
+            item(2) eq 5
+        }
+    }
+
+    @Test
+    fun `call in batch default size`() {
+        val list = mutableListOf(1)
+        repeat(100) {
+            list.add(1)
+        }
+        CollectionHelper.batchCall(list) {
+            it.count()
+        } verify {
+            item(0) eq 100
+            item(1) eq 1
+        }
+    }
+
+    @Test
+    fun `consume in batch`() {
+        val result = mutableListOf<Int>()
+        CollectionHelper.batchConsume(listOf(1, 2)) {
+            result.addAll(it)
+        }
+
+        result verify {
+            item(0) eq 1
+            item(1) eq 2
+        }
+
+        result.clear()
+
+        CollectionHelper.batchConsume(listOf(1, 2, 3), 2) {
+            result.add(it.count())
+        }
+
+        result verify {
+            item(0) eq 2
+            item(1) eq 1
+        }
+
+        result.clear()
+
+        CollectionHelper.batchConsume(listOf(1, 2, 3, 4), 2) {
+            result.add(it.count())
+        }
+
+        result verify {
+            item(0) eq 2
+            item(1) eq 2
+        }
+
+        result.clear()
+
+        CollectionHelper.batchConsume(listOf(1), 2) {
+            result.add(it.count())
+        }
+
+        result verify {
+            item(0) eq 1
+        }
+    }
+
+    @Test
+    fun `consume in batch default size`() {
+        val list = mutableListOf<Int>()
+        repeat(101) {
+            list.add(1)
+        }
+
+        val result = mutableListOf<Int>()
+        CollectionHelper.batchConsume(list) {
+            result.add(it.count())
+        }
+
+        result verify {
+            item(0) eq 100
+            item(1) eq 1
+        }
+    }
+}
